b1f8dc4e4bf2363aa1d9c1635631534f359ef98a
Brady Peterson <bpeterson1663@gmail.com>
2024-02-10 07:48:11 -0600

    feat(pdf): combine pdfs into one

--- PDF/main.py
+++ PDF/main.py
@@ -1,9 +1,16 @@
 import PyPDF2
 import os
+import sys
 
+inputs = sys.argv[1:]
 cwd = os.getcwd()
 
-print(cwd)
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+    merger.write('docs/all.pdf')
+    
 
 with open(os.path.join(cwd, "docs/dummy.pdf"), 'rb') as file:
     reader = PyPDF2.PdfFileReader(file)
@@ -13,3 +20,8 @@ with open(os.path.join(cwd, "docs/dummy.pdf"), 'rb') as file:
     writer.addPage(page)
     with open(os.path.join(cwd, 'docs/tilt.pdf'), 'wb') as new_file:
         writer.write(new_file)
+        
+pdf_combiner(inputs)
+
+
+# python3 main.py docs/dummy.pdf docs/twopage.pdf docs/tilt.pd
